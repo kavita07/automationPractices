31c28a39a45cf8eff6aaedf8a4b24f59ab47657d
unknown <kavita.chavan07@gmail.com>
2021-05-05 21:56:50 +0530

    added test file

---
+++ wipro.com/src/main/java/cucmber_automation/wipro/com/App.java
@@ -0,0 +1,13 @@
+package cucmber_automation.wipro.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ wipro.com/src/test/java/cucmber_automation/wipro/com/test.java
@@ -0,0 +1,10 @@
+package cucmber_automation.wipro.com;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hellow world");
+	}
+
+}
